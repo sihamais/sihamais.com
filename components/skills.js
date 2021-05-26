757ca250e15b21341be7c29cfe785f83990ab00f
Sihamais <sihamais98@gmail.com>
2021-05-26 06:43:58 +0200

    Add skills

--- components/skills.js
+++ components/skills.js
@@ -7,10 +7,17 @@ export default function Skills() {
       id="skills"
       className="w-screen lg:min-h-screen md:h-auto py-20 lg:px-20 bg-gray-100 space-y-10 dark:bg-gray-700"
     >
-      <div className="lg:text-center pl-8">
-        <h2 className="text-xl text-gray-900 dark:text-white font-bold tracking-wide uppercase">
-          Technical Skills
-        </h2>
+      <div className="grid grid-cols-2">
+        <div className="lg:text-center pl-8">
+          <h2 className="text-xl text-gray-900 dark:text-white font-bold tracking-wide uppercase">
+            Technical Skills
+          </h2>
+        </div>
+        <div className="lg:text-center pl-8">
+          <h2 className="text-xl text-gray-900 dark:text-white font-bold tracking-wide uppercase">
+            Soft Skills
+          </h2>
+        </div>
       </div>
     </div>
   );
